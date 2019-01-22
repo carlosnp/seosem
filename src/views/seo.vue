2d3018fd4043c9517845a36cc4e68fdb401ac038
carlosnp <machinery.e@gmail.com>
2019-01-22 17:35:31 -0400

    panorama digital

---
+++ src/views/seo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class='visually_embed'>
+        <img class='visually_embed_infographic' src='https://visual.ly/node/image/150315?_w=540' alt='The Internet In Real Time' />
+        <div class='visually_embed_cycle'></div>
+        <p> From <a href='https://visual.ly?utm_source=content-embed&utm_medium=embed'>Visually</a>.</p>
+    </div>
+</template>
+
+<script 
+type='text/javascript' 
+src='https://a.visual.ly/api/embed/150315?width=540' 
+class='visually_embed_script' 
+id='visually_embed_script_150315'
+>
+</script>
