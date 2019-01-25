e3b9ed955b5ded0feb3c349ad2bf3bcfb258d8bd
carlosnp <machinery.e@gmail.com>
2019-01-25 09:29:17 -0400

    prueba deploy dos

--- src/router.js
+++ src/router.js
@@ -10,7 +10,7 @@ import seosem from './views/seosem.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes: [
     {
