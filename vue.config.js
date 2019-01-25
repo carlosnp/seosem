faead897f02e110f54b384d09196b3cbb5700e9f
carlosnp <machinery.e@gmail.com>
2019-01-25 10:50:19 -0400

    prueba deploy 4

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   //Configuracion para al momento de correr el npm run build ajustar el sitio al que apuntara
-  baseUrl: process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === 'production'
     ? './dist/'
     : '/',
   //Esta configuracion sirve para generar el index.html en la raiz
